9826c66ed5907fcab90002402405d3b90d8eb491
Jonas Scharpf <jonas@brainelectronics.de>
2022-07-21 10:58:06 +0200

    add NexGauge class for Nextion gauges

---
+++ nextion/nextion_gauge.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+"""
+NexGauge
+
+Functions to interact with a Nextion Gauge element
+"""
+
+# custom packages
+from .common import Common, CommonValueMixin
+
+
+class NexGaugeError(Exception):
+    """Base class for exceptions in this module."""
+    pass
+
+
+class NexGauge(Common, CommonValueMixin):
+    """docstring for NexGauge"""
+    def __init__(self, nh, pid: int, cid: int, name: str) -> None:
+        """
+        Init gauge
+
+        :param      nh:    The Nextion hardware interface object
+        :type       nh:    NexHardware
+        :param      pid:   The page ID
+        :type       pid:   int
+        :param      cid:   The component ID
+        :type       cid:   int
+        :param      name:  The component name
+        :type       name:  str
+        """
+        super().__init__(nh, pid, cid, name)
