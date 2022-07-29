2e494ee1a9b375a33f4143f5e235cfc61583c0b1
Jonas Scharpf <jonas@brainelectronics.de>
2022-07-29 08:32:41 +0200

    extend button example with background color and font functions

--- examples/button/main.py
+++ examples/button/main.py
@@ -28,8 +28,10 @@ nh.nexInit()
 # create a button instance
 b0 = NexButton(nh, 0, 1, "b0")
 
-# new text of button
+# new values of button
 button_text = "btn txt"
+background_color_value = 63488  # red
+font_color_value = 31           # blue
 
 # request the text of button "b0"
 print('Requesting button "{}" text ...'.format(b0.name))
@@ -54,6 +56,64 @@ print()
 
 # sanity check
 if response != button_text:
+    print('WARNING: GET value did not match SET value')
+
+time.sleep(1)
+
+# request the background color of button "b0"
+print('Requesting background color of button "{}" ...'.format(b0.name))
+response = b0.Get_background_color_bco()
+print('Background color of button "{}" is: "{}"'.format(b0.name, response))
+print()
+
+time.sleep(1)
+
+# modify the background color of button "b0" to "red"
+# search for RGB565 Colors. Red is "63488" at 65k colors
+print('Set background color of button "{}" to "{}"'.
+      format(b0.name, background_color_value))
+b0.Set_background_color_bco(background_color_value)
+print()
+
+time.sleep(1)
+
+# request the background color of button "b0" again
+print('Requesting background color of button "{}" ...'.format(b0.name))
+response = b0.Get_background_color_bco()
+print('Background color of button "{}" is: "{}"'.format(b0.name, response))
+print()
+
+# sanity check
+if response != background_color_value:
+    print('WARNING: GET value did not match SET value')
+
+time.sleep(1)
+
+# request the font color of button "b0"
+print('Requesting font color of button "{}" ...'.format(b0.name))
+response = b0.Get_font_color_pco()
+print('Font color of button "{}" is: "{}"'.format(b0.name, response))
+print()
+
+time.sleep(1)
+
+# modify the font color of button "b0" to "blue"
+# search for RGB565 Colors. Blue is "31" at 65k colors
+print('Set font color of button "{}" to "{}"'.
+      format(b0.name, font_color_value))
+b0.Set_font_color_pco(font_color_value)
+print()
+
+time.sleep(1)
+
+# request the font color of button "b0" again
+print('Requesting font color of button "{}" ...'.format(b0.name))
+response = b0.Get_font_color_pco()
+print('Font color of button "{}" is: "{}"'.format(b0.name, response))
+print()
+
+# sanity check
+if response != font_color_value:
     print('WARNING: GET value did not match SET value')
 
 print('Returning to REPL in 5 seconds')
