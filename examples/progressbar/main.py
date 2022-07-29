f2b9f1aee063cf2cb84f657ba595d884b74c8625
Jonas Scharpf <jonas@brainelectronics.de>
2022-07-29 08:54:59 +0200

    extend progressbar example with background color and font functions

--- examples/progressbar/main.py
+++ examples/progressbar/main.py
@@ -29,8 +29,11 @@ nh.nexInit()
 # create a progressbar instance
 j0 = NexProgressBar(nh, 0, 1, "j0")
 
-# new value of progressbar, avoid something close to zero or close to 50
+# new values of progressbar
+# avoid something close to zero or close to 50
 progressbar_value = choice([randint(5, 40), randint(60, 100)])
+background_color_value = 63488  # red
+font_color_value = 31           # blue
 
 # request the value of progressbar "j0" being 50 by default
 print('Requesting progressbar "{}" value ...'.format(j0.name))
@@ -55,6 +58,66 @@ print()
 
 # sanity check
 if response != progressbar_value:
+    print('WARNING: GET value did not match SET value')
+
+time.sleep(1)
+
+# request the background color of progressbar "j0"
+print('Requesting background color of progressbar "{}" ...'.format(j0.name))
+response = j0.Get_background_color_bco()
+print('Background color of progressbar "{}" is: "{}"'.
+      format(j0.name, response))
+print()
+
+time.sleep(1)
+
+# modify the background color of progressbar "j0" to "red"
+# search for RGB565 Colors. Red is "63488" at 65k colors
+print('Set background color of progressbar "{}" to "{}"'.
+      format(j0.name, background_color_value))
+j0.Set_background_color_bco(background_color_value)
+print()
+
+time.sleep(1)
+
+# request the background color of progressbar "j0" again
+print('Requesting background color of progressbar "{}" ...'.format(j0.name))
+response = j0.Get_background_color_bco()
+print('Background color of progressbar "{}" is: "{}"'.
+      format(j0.name, response))
+print()
+
+# sanity check
+if response != background_color_value:
+    print('WARNING: GET value did not match SET value')
+
+time.sleep(1)
+
+# request the font color of progressbar "j0"
+print('Requesting font color of progressbar "{}" ...'.format(j0.name))
+response = j0.Get_font_color_pco()
+print('Font color of progressbar "{}" is: "{}"'.format(j0.name, response))
+print()
+
+time.sleep(1)
+
+# modify the font color of progressbar "j0" to "blue"
+# search for RGB565 Colors. Blue is "31" at 65k colors
+print('Set font color of progressbar "{}" to "{}"'.
+      format(j0.name, font_color_value))
+j0.Set_font_color_pco(font_color_value)
+print()
+
+time.sleep(1)
+
+# request the font color of progressbar "j0" again
+print('Requesting font color of progressbar "{}" ...'.format(j0.name))
+response = j0.Get_font_color_pco()
+print('Font color of progressbar "{}" is: "{}"'.format(j0.name, response))
+print()
+
+# sanity check
+if response != font_color_value:
     print('WARNING: GET value did not match SET value')
 
 print('Returning to REPL in 5 seconds')
