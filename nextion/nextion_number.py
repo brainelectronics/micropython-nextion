6f824a2c7d806211eebd62eb07165fec766cc1b1
Jonas Scharpf <jonas@brainelectronics.de>
2022-07-30 08:45:22 +0200

    support all class specific functions of number

--- nextion/nextion_number.py
+++ nextion/nextion_number.py
@@ -7,6 +7,9 @@ NexNumber
 Functions to interact with a Nextion Number element
 """
 
+# system packages
+from time import sleep
+
 # custom packages
 from .common import Common, CommonBackgroundColorMixin, CommonFontMixin, CommonPositionMixin, CommonValueMixin
 
@@ -32,3 +35,31 @@ class NexNumber(Common, CommonBackgroundColorMixin, CommonFontMixin, CommonPosit
         :type       name:  str
         """
         super().__init__(nh, pid, cid, name)
+
+    def Get_number_length(self) -> int:
+        """
+        Get length attribute of component
+
+        :returns:   The number length
+        :rtype:     int
+        """
+        cmd = "get {}.lenth".format(self.name)
+        self._nh.sendCommand(cmd)
+        sleep(0.1)  # necessary, data might not be available otherwise
+        return self._nh.recvRetNumber()
+
+    def Set_number_length(self, number: int) -> bool:
+        """
+        Set length attribute of component
+
+        :param      number:  The number length
+        :type       number:  int
+
+        :returns:   True on success, false otherwise
+        :rtype:     bool
+        """
+        cmd = "{}.lenth={}".format(self.name, number)
+        self._nh.sendCommand(cmd)
+        cmd = "ref {}".format(self.name)
+        self._nh.sendCommand(cmd)
+        return self._nh.recvRetCommandFinished()
