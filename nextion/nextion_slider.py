5e0627337876458c1ccf5ca5627856b9d11e0f20
Jonas Scharpf <jonas@brainelectronics.de>
2022-07-30 08:58:28 +0200

    support all class specific functions of slider

--- nextion/nextion_slider.py
+++ nextion/nextion_slider.py
@@ -7,6 +7,9 @@ NexSlider
 Functions to interact with a Nextion Slider element
 """
 
+# system packages
+from time import sleep
+
 # custom packages
 from .common import Common, CommonBackgroundColorMixin, CommonFontMixin, CommonPointerMixin, CommonValueMixin
 
@@ -32,3 +35,87 @@ class NexSlider(Common, CommonBackgroundColorMixin, CommonFontMixin, CommonPoint
         :type       name:  str
         """
         super().__init__(nh, pid, cid, name)
+
+    def Get_cursor_height_hig(self) -> int:
+        """
+        Get cursor hig attribute of component
+
+        :returns:   The cursor height.
+        :rtype:     number
+        """
+        cmd = "get {}.hig".format(self.name)
+        self._nh.sendCommand(cmd)
+        sleep(0.1)  # necessary, data might not be available otherwise
+        return self._nh.recvRetNumber()
+
+    def Set_cursor_height_hig(self, number: int) -> bool:
+        """
+        Set cursor hig attribute of component
+
+        :param      number:  The number
+        :type       number:  int
+
+        :returns:   True on success, false otherwise
+        :rtype:     bool
+        """
+        cmd = "{}.hig={}".format(self.name, number)
+        self._nh.sendCommand(cmd)
+        cmd = "ref {}".format(self.name)
+        self._nh.sendCommand(cmd)
+        return self._nh.recvRetCommandFinished()
+
+    def getMaxval(self) -> int:
+        """
+        Get the maxval attribute of the component
+
+        :returns:   The maxval
+        :rtype:     int
+        """
+        cmd = "get {}.maxval".format(self.name)
+        self._nh.sendCommand(cmd)
+        sleep(0.1)  # necessary, data might not be available otherwise
+        return self._nh.recvRetNumber()
+
+    def setMaxval(self, number: int) -> bool:
+        """
+        Set the maxval attribute of the component
+
+        :param      number:  The new maxval
+        :type       number:  int
+
+        :returns:   True on success, false otherwise
+        :rtype:     bool
+        """
+        cmd = "{}.maxval={}".format(self.name, number)
+        self._nh.sendCommand(cmd)
+        cmd = "ref {}".format(self.name)
+        self._nh.sendCommand(cmd)
+        return self._nh.recvRetCommandFinished()
+
+    def getMinval(self) -> int:
+        """
+        Get the minval attribute of the component
+
+        :returns:   The minval
+        :rtype:     int
+        """
+        cmd = "get {}.minval".format(self.name)
+        self._nh.sendCommand(cmd)
+        sleep(0.1)  # necessary, data might not be available otherwise
+        return self._nh.recvRetNumber()
+
+    def setMinval(self, number: int) -> bool:
+        """
+        Set the minval attribute of the component
+
+        :param      number:  The new minval
+        :type       number:  int
+
+        :returns:   True on success, false otherwise
+        :rtype:     bool
+        """
+        cmd = "{}.minval={}".format(self.name, number)
+        self._nh.sendCommand(cmd)
+        cmd = "ref {}".format(self.name)
+        self._nh.sendCommand(cmd)
+        return self._nh.recvRetCommandFinished()
