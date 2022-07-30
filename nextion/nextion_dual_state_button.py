5e455b1b25eb4c4c450a84dd7c9f9f4597652be1
Jonas Scharpf <jonas@brainelectronics.de>
2022-07-30 08:33:39 +0200

    support all class specific functions of dual state button

--- nextion/nextion_dual_state_button.py
+++ nextion/nextion_dual_state_button.py
@@ -7,6 +7,9 @@ NexDSButton
 Functions to interact with a Nextion dual state Button element
 """
 
+# system packages
+from time import sleep
+
 # custom packages
 from .common import Common, CommonFontMixin, CommonPositionMixin, CommonTextMixin, CommonValueMixin
 
@@ -32,3 +35,59 @@ class NexDSButton(Common, CommonFontMixin, CommonPositionMixin, CommonTextMixin,
         :type       name:  str
         """
         super().__init__(nh, pid, cid, name)
+
+    def Get_state0_color_bco0(self) -> int:
+        """
+        Get bco0 attribute of component
+
+        :returns:   The background color
+        :rtype:     int
+        """
+        cmd = "get {}.bco0".format(self.name)
+        self._nh.sendCommand(cmd)
+        sleep(0.1)  # necessary, data might not be available otherwise
+        return self._nh.recvRetNumber()
+
+    def Set_state0_color_bco0(self, number: int) -> bool:
+        """
+        Set bco0 attribute of component
+
+        :param      number:  The background color number
+        :type       number:  int
+
+        :returns:   True on success, false otherwise
+        :rtype:     bool
+        """
+        cmd = "{}.bco0={}".format(self.name, number)
+        self._nh.sendCommand(cmd)
+        cmd = "ref {}".format(self.name)
+        self._nh.sendCommand(cmd)
+        return self._nh.recvRetCommandFinished()
+
+    def Get_state1_color_bco1(self) -> int:
+        """
+        Get bco1 attribute of component
+
+        :returns:   The background color
+        :rtype:     int
+        """
+        cmd = "get {}.bco1".format(self.name)
+        self._nh.sendCommand(cmd)
+        sleep(0.1)  # necessary, data might not be available otherwise
+        return self._nh.recvRetNumber()
+
+    def Set_state1_color_bco1(self, number: int) -> bool:
+        """
+        Set bco1 attribute of component
+
+        :param      number:  The background color number
+        :type       number:  int
+
+        :returns:   True on success, false otherwise
+        :rtype:     bool
+        """
+        cmd = "{}.bco1={}".format(self.name, number)
+        self._nh.sendCommand(cmd)
+        cmd = "ref {}".format(self.name)
+        self._nh.sendCommand(cmd)
+        return self._nh.recvRetCommandFinished()
