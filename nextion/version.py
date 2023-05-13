34e6114ee416515c83c9b59f47d4a346b91ac1f9
Daniel Herrmann <daniel.herrmann1@gmail.com>
2023-05-13 12:31:53 +0200

    Update version.py

--- nextion/version.py
+++ nextion/version.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
-__version_info__ = ('0', '4', '0')
+__version_info__ = ('0', '14', '0')
 __version__ = '.'.join(__version_info__)
 __author__ = 'brainelectronics'
